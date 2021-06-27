988daf6c5646d61d95659713be5d3f995e6df7d7
arpita14378 <arpitahk@gmail.com>
2021-06-28 01:39:43 +0530

    chain code

--- block.js
+++ block.js
@@ -13,4 +13,41 @@ class Block{
     }
 }
 
-//Aaditya
+class BlockChain{
+    constructor(){
+        this.blockchain = this.headBlock()
+    }
+    headBlock(){
+        return new Block({})
+    }
+    prevBlock(){
+        return this.blockchain[this.blockchain.length -1]
+    }
+    addNewBlock(newBlock){
+        newBlock.prevHash = this.prevBlock().hash
+        newBlock.hash = newBlock.getHash()
+        this.blockchain.push(newBlock)
+    }
+    chainValidity(){
+        for(let i=1;i<this.blockchain.length;i++){
+            const currb = this.blockchain[i]
+            const prevb = this.blockchain[i-1]
+            if(currb.hash != currb.getHash()){
+                return false
+            }
+            if(prevb.hash != this.prevBlock.hash){
+                return false
+            }
+            return true
+        }
+    }
+
+}
+
+
+let a = new Block({from: "Arpita", to: "Aaditya"})
+
+let chain = new BlockChain()
+chain.addNewBlock(a)
+console.log(chain)
+console.log("Validity: " + chain.chainValidity())
